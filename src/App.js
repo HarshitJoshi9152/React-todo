a64818f2322a39ac8454623fbd1517147e73281f
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-01-27 20:37:44 +0530

    tutorial almost finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Table from "./Table"
+import Form from "./Form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+	state = {
+		characters: []
+	};
+
+	removeCharacters = (index) => {
+		const {characters} = this.state;
+
+		this.setState({
+			characters: characters.filter( (character, i) => {
+				return i !== index
+			})
+		})
+	}
+
+	handleSubmit = (newChar) => {
+		this.setState({
+			characters: [...this.state.characters, newChar]
+		});
+	}
+
+	render() {
+		const {characters} = this.state
+		return (
+			<>
+			<header className="hero">
+				<h1>Hello world</h1>
+				<p>class vs className!</p>
+			</header>
+			<div>
+				<Table characterData={characters} removeCharacters={this.removeCharacters} />
+				<Form handleSubmit={this.handleSubmit}/>
+			</div>
+			</>
+		)
+	}
 }
 
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class Table extends Component{
+	render () {
+		const {characterData, removeCharacters} = this.props;
+
+		return (
+			<table>
+				<TableHead/>
+				<TableBody characterData={characterData} removeCharacters={removeCharacters} />
+			</table>
+		)
+	}
+}
+
+function TableHead() {
+	return (
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Job</th>
+				<th>Remove</th>
+			</tr>
+		</thead>
+	);
+}
+
+function TableBody(props) {
+	const rows = props.characterData.map( (row, index) => {
+		return (
+			<tr key={index}>
+				<td> {row.name} </td>
+				<td> {row.job} </td>
+				<td>
+					<button onClick={ () => props.removeCharacters(index)}>Delete</button>
+				</td>
+			</tr>
+		)
+	});
+	return <tbody> { rows } </tbody>;
+}
+
+
+export default Table;
